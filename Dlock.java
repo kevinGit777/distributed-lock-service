733260d91d8182e2e79b675cdc0073cb6fcd3f39
CMJMJ <fl-239663@qq.com>
2021-12-01 16:47:56 -0600

    Update Dlock.java
    part 1 constructor, lock and unlock.

--- Dlock.java
+++ Dlock.java
@@ -1,13 +1,37 @@
 import java.util.Deque;
 import java.util.concurrent.locks.Lock;
 
-public class Dlock {
     Node node;
     Deque<Message> queue;
     int current_request_timestamp;
     Lock lock;
     NodeID[] neighbors;
     boolean[] recieve_neighbor;
+
+    public Dlock(NodeID identifier, String configFileName){
+            this.node = Node(identifier, configFileName,);// Here is a error with the third parameter.
+        this.current_request_timestamp = -1;
+        int nodeNum = node.node_num;
+        this.neighbors = node.getNeighbors();
+        for (int i = 0;i < nodeNum;i++){
+            recieve_neighbor[i] = false;
+        }
+    }
+
+    public void lock(int time_stamp){
+        Message requestMessage = makeMessage("request");
+        node.sendToAll(requestMessage);
+        waitForAllNeighborReply();
+    }
+
+    public void unlock(){
+        lock.lock();
+        current_request_timestamp = -1;
+        for ( Message message : queue) {
+            node.send(makeMessage("reply"), message.source);
+        }
+        lock.unlock();
+    }
 
     synchronized void recieve(Message message)
     {
@@ -16,7 +40,6 @@ public class Dlock {
             lock.lock();
             if(current_request_timestamp == -1 || message.timestamp < current_request_timestamp)
                 {
-                    node.send(makeMessage("reply"), message.source);
             }else
             {
                queue.push(message); 
